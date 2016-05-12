a153d027054ab658b3909956416cdb7e070e5f81
Niko Solihin <nikosolihin@gmail.com>
2016-05-12 14:21:07 +0700

    Add postcss

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -5,11 +5,20 @@ var gulp         = require('gulp')
 var gulpif       = require('gulp-if')
 var browserSync  = require('browser-sync')
 var sass         = require('gulp-sass')
+var postcss      = require('gulp-postcss')
 var sourcemaps   = require('gulp-sourcemaps')
 var handleErrors = require('../lib/handleErrors')
-var autoprefixer = require('gulp-autoprefixer')
 var path         = require('path')
-var cssnano      = require('gulp-cssnano')
+
+// PostCSS processors
+var bem             = require('postcss-bem')
+var short           = require('postcss-short')
+var clearfix        = require('postcss-clearfix')
+var responsiveType  = require('postcss-responsive-type')
+var quantityQueries = require('postcss-quantity-queries')
+var autoprefixer    = require('autoprefixer')(config.tasks.css.autoprefixer)
+var cssnano         = require('cssnano')
+var cssnext         = require('postcss-cssnext')
 
 var paths = {
   src: path.join(config.root.src, config.tasks.css.src, '/**/*.{' + config.tasks.css.extensions + '}'),
@@ -17,12 +26,19 @@ var paths = {
 }
 
 var cssTask = function () {
+  var processors = [
+    bem,
+    short,
+    clearfix,
+    responsiveType,
+    quantityQueries,
+    autoprefixer,
+    cssnano
+  ]
   return gulp.src(paths.src)
     .pipe(gulpif(!global.production, sourcemaps.init()))
     .pipe(sass(config.tasks.css.sass))
-    .on('error', handleErrors)
-    .pipe(autoprefixer(config.tasks.css.autoprefixer))
-    .pipe(gulpif(global.production, cssnano({autoprefixer: false})))
+    .pipe(postcss( processors )).on('error', handleErrors)
     .pipe(gulpif(!global.production, sourcemaps.write()))
     .pipe(gulp.dest(paths.dest))
     .pipe(browserSync.stream())
